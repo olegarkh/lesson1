65ca53e15e3b94946f78bb9ee32d3ef99a547630
Oleg <oarch@mail.ru>
2015-02-18 17:42:37 +0300

    Показ одной статьи целиком

--- views/article.php
+++ views/article.php
@@ -1,10 +1,22 @@
+<?php
+
+require_once __DIR__.'/../models/articles.php';
+$article = get_article($_GET['id']);
+//$article = get_article(1);
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head lang="en">
   <meta charset="UTF-8">
-  <title></title>
+  <title><?php $article['title']?></title>
 </head>
 <body>
+    <p><a href="../index.php">На главную</a></p>
+    <h3><?php echo $article['title'];?></h3>
+    <p><?php echo $article['pub_date']?></p>
+    <p><?php echo $article['article']?></p>
 
 </body>
 </html>
