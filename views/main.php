e48eb02cb91eb5b9bce85d9744ae6907bd9bc2f6
Oleg <oarch@mail.ru>
2015-02-18 17:48:38 +0300

    Список всех статей отсортированных по дате в обратном порядке

--- views/main.php
+++ views/main.php
@@ -9,13 +9,16 @@
 <html>
 <head lang="en">
     <meta charset="UTF-8">
-    <title></title>
+    <title>Список статей</title>
 </head>
 <body>
+  <a href="load.php">Добавить статью</a>
   <?php foreach ($items as $item):?>
-      <h3><?php echo $item['name'];?></h3>
-      <p><?php echo $item['date'];?></p>
-      <p><?php echo $item['content'];?></p>
+      <h3><?php echo $item['title'];?></h3>
+      <p><?php echo $item['pub_date'];?></p>
+      <p><?php echo $item['preview'];?></p>
+      <a href="views/article.php<?php echo "?id=".$item['id'];?>"> Читать далее...</a>
+      <a href="views/load.php<?php echo "?id=".$item['id'];?>"> Редактировать статью</a>
   <?php endforeach; ?>
 </body>
 </html>
