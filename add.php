e059a9e7f886cb8d218fd20f855cbb881194a143
Oleg <oarch@mail.ru>
2015-02-18 17:51:53 +0300

    Контроллер добавления статей в БД

--- add.php
+++ add.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Олег
- * Date: 18.02.15
- * Time: 8:05
- */ 
+
+//require __DIR__.'/functions/sql.php';
+require __DIR__.'/models/articles.php';
+
+$pub_date = $_POST['date'];
+$title = $_POST['title'];
+$preview = $_POST['preview'];
+$article = $_POST['article'];
+
+
+$res = record_toDB($pub_date, $title, $preview, $article);
+
+
+if ($res){
+    header('Location: index.php');
+}
+else {
+    header('Location:/views/load.php');
+}
