019c4b6df4c8d182c7b262195eeb3a2c6bdc0089
Oleg <oarch@mail.ru>
2015-02-18 20:43:23 +0300

    Контроллер добавления новостей

--- add.php
+++ add.php
@@ -2,16 +2,16 @@
 
 require __DIR__.'/models/articles.php';
 
-$pub_date = $_POST['date'];
+$pub_date = $_POST['date'];                    //Получаем из формы информацию по новости
 $title = $_POST['title'];
 $preview = $_POST['preview'];
 $article = $_POST['article'];
 
-$res = record_toDB($pub_date, $title, $preview, $article);
+$res = record_toDB($pub_date, $title, $preview, $article); // отсылаем в БД
 
 if ($res){
-    header('Location: index.php');
+    header('Location: index.php');              // отправляемся на главную страницу
 }
 else {
-    header('Location: /views/load.php');
+    header('Location: /views/load.php');        // в случае неудачи остаемся редактировать новость
 }
