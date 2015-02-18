8b81f38a522ab33d7a76b6fb00df5c6007a16c82
Oleg <oarch@mail.ru>
2015-02-18 20:36:00 +0300

    Контроллер главной страницы

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__. '/models/articles.php';
 
-$items = articles_getAll();
+$items = articles_getAll();                  // Получаем из БД все статьи
 
-include __DIR__.'/views/main.php';
+include __DIR__.'/views/main.php';          // и переходим на страницу их отображения
 
