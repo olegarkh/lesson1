42a7cd676ec48ad070c07953ee132eb778926f5d
Oleg <oarch@mail.ru>
2015-02-18 17:43:54 +0300

    Форма добавления новости на сайт

--- views/load.php
+++ views/load.php
@@ -2,9 +2,16 @@
 <html>
 <head lang="en">
   <meta charset="UTF-8">
-  <title></title>
+  <title>Загрузить статью</title>
 </head>
 <body>
-
+   <p><a href="../index.php">На главную</a></p>
+   <form action="../add.php" method="post">
+       <p><label>Введите дату<br><input type="date" name="date"></label></p>
+       <p><label>Введите название статьи<br><input type="text" name="title" size="100"></label></p>
+       <p><label>Превью для статьи<br><textarea name="preview" rows="7" cols="80"></textarea></label></p>
+       <p><label>Текст статьи<br><textarea name="article" rows="15" cols="80"></textarea></label></p>
+       <p><input type="submit" name="sub" value="Отправить"></p>
+   </form>
 </body>
 </html>
